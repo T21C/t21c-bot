45938e54676500eb2638da052e66552ba695e903
Phrygia / sjk <sungntung@gmail.com>
2026-01-02 15:46:30 +0900

    feat: deafen

---
+++ src/commands/deafen.ts
@@ -0,0 +1,40 @@
+import {
+    SlashCommandBuilder,
+    EmbedBuilder,
+    PermissionFlagsBits,
+    Interaction,
+    ChatInputCommandInteraction
+} from 'discord.js'
+import '../index'
+
+module.exports = {
+    tufOnly: true,
+    data: new SlashCommandBuilder()
+        .setName('deafen')
+        .setDescription('Deafen a member in a good run.')
+        .addUserOption((option) =>
+            option
+                .setName('user')
+                .setDescription('The user you want to deafen')
+                .setRequired(true)
+        ),
+    execute(interaction: ChatInputCommandInteraction) {
+        const user = interaction.options.getUser('user', true)
+
+        const member = interaction.guild.members.cache.get(user.id)
+        if (!member) {
+            return interaction.reply({
+                ephemeral: true,
+                content: 'The user is not in TUF.'
+            })
+        }
+        if (!member.voice) {
+            return interaction.reply({
+                content: 'The member is not in a voice channel.'
+            })
+        }
+
+        member.voice.setDeaf(true, `/deafen by ${interaction.user.username}`)
+        return interaction.reply({ content: `${user} has been deafened.` })
+    }
+}
